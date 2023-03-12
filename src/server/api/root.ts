6d3a4d604ff8fb1235169f5dbb6a75a61708e756
Reynan Valencia <reynanvalencia@Reynans-MacBook-Pro.local>
2023-03-12 16:06:31 +0900

    Add Notes, Topics, etc

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { topicRouter } from "~/server/api/routers/topic"
+import { noteRouter } from "~/server/api/routers/note"
 
 /**
  * This is the primary router for your server.
@@ -8,6 +10,8 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  topic: topicRouter,
+  note: noteRouter,
 });
 
 // export type definition of API
